7710cce0ddce1d5d61147e1569e141f336a76751
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2022-11-16 20:06:48 -0300

    atualizei

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,346 @@
 function fechar() {
-    window.open('', '_self', '').close()
+    var setavoltar = document.getElementById("setavoltar")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        setavoltar.classList.add("setavoltarOnClick")
+        setTimeout(() => {
+            window.open('', '_self', '').close()
+        }, 1000);
+        setTimeout(() => {
+            setavoltar.classList.remove("setavoltarOnClick")
+        }, 500);
+    } else {
+        window.open('', '_self', '').close()
+    }
+}
+
+/*let imgperdiv = document.getElementById("imgperdiv")
+let imgpernor = document.getElementById("imgpernor")
+let imgperp = document.getElementById("imgperp")
+function ampliar() {
+    let imgpernordiv = document.getElementById("imgpernordiv")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgpernordiv.classList.add("imgpernordivOnClick")
+        setTimeout(() => {
+            imgperdiv.style.display = "flex"
+            imgpernor.style.display = "none"
+            imgperp.style.display = "none"
+            
+        }, 1000)
+        setTimeout(() => {
+            imgpernordiv.classList.remove("imgpernordivOnClick")
+        }, 500)
+    } else {
+        imgperdiv.style.display = "flex"
+        imgpernor.style.display = "none"
+        imgperp.style.display = "none"
+    }
+}
+function minimizar() {
+    let imgperampdiv = document.getElementById("imgperampdiv")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgperampdiv.classList.add("imgperampdivOnClick")
+        setTimeout(() => {
+            imgperdiv.style.display = "none"
+            imgperp.style.display = "inline-block"
+            imgpernor.style.display = "inline-block"
+        }, 1000)
+        setTimeout(() => {
+            imgperampdiv.classList.remove("imgperampdivOnClick")
+        }, 500)
+    } else {
+        imgperdiv.style.display = "none"
+        imgperp.style.display = "inline-block"
+        imgpernor.style.display = "inline-block"
+       
+    }
+}*/
+
+let imgperdiv = document.getElementById("imgperdiv")
+let imgpernor = document.getElementById("imgpernor")
+let imgperp = document.getElementById("imgperp")
+let imgperdiv2 = document.getElementById("imgperdiv2")
+let imgpernor2 = document.getElementById("imgpernor2")
+let imgperp2 = document.getElementById("imgperp2")
+let imgperdiv3 = document.getElementById("imgperdiv3")
+let imgpernor3 = document.getElementById("imgpernor3")
+let imgperp3 = document.getElementById("imgperp3")
+let imgperdiv4 = document.getElementById("imgperdiv4")
+let imgpernor4 = document.getElementById("imgpernor4")
+let imgperp4 = document.getElementById("imgperp4")
+
+function ampliar() {
+    let imgpernordiv = document.getElementById("imgpernordiv")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgpernordiv.classList.add("imgpernordivOnClick")
+        setTimeout(() => {
+            imgperdiv.style.display = "flex"
+            document.body.style.overflow = "hidden"
+            imgpernor.style.display = "none"
+            imgperp.style.display = "none"
+            imgpernor2.style.display = "none"
+            imgperp2.style.display = "none"
+            imgpernor3.style.display = "none"
+            imgperp3.style.display = "none"
+            imgpernor4.style.display = "none"
+            imgperp4.style.display = "none"
+
+
+        }, 1000)
+        setTimeout(() => {
+            imgpernordiv.classList.remove("imgpernordivOnClick")
+        }, 500)
+    } else {
+        imgperdiv.style.display = "flex"
+        document.body.style.overflow = "hidden"
+        imgpernor.style.display = "none"
+        imgperp.style.display = "none"
+        imgpernor2.style.display = "none"
+        imgperp2.style.display = "none"
+        imgpernor3.style.display = "none"
+        imgperp3.style.display = "none"
+        imgpernor4.style.display = "none"
+        imgperp4.style.display = "none"
+        
+    }
+}
+function minimizar() {
+    let imgperampdiv = document.getElementById("imgperampdiv")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgperampdiv.classList.add("imgperampdivOnClick")
+        setTimeout(() => {
+            imgperdiv.style.display = "none"
+            document.body.style.overflow = "visible"
+            imgperp.style.display = "inline-block"
+            imgpernor.style.display = "inline-block"
+            imgperp2.style.display = "inline-block"
+            imgpernor2.style.display = "inline-block"
+            imgperp3.style.display = "inline-block"
+            imgpernor3.style.display = "inline-block"
+            imgperp4.style.display = "inline-block"
+            imgpernor4.style.display = "inline-block"
+            
+        }, 1000)
+        setTimeout(() => {
+            imgperampdiv.classList.remove("imgperampdivOnClick")
+        }, 500)
+    } else {
+        imgperdiv.style.display = "none"
+        document.body.style.overflow = "visible"
+        imgperp.style.display = "inline-block"
+        imgpernor.style.display = "inline-block"
+        imgperp2.style.display = "inline-block"
+        imgpernor2.style.display = "inline-block"
+        imgperp3.style.display = "inline-block"
+        imgpernor3.style.display = "inline-block"
+        imgperp4.style.display = "inline-block"
+        imgpernor4.style.display = "inline-block"
+
+    }
+}
+function ampliar2() {
+    let imgpernordiv2 = document.getElementById("imgpernordiv2")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgpernordiv2.classList.add("imgpernordivOnClick")
+        setTimeout(() => {
+            imgperdiv2.style.display = "flex"
+            document.body.style.overflow = "hidden"
+            imgpernor.style.display = "none"
+            imgperp.style.display = "none"
+            imgpernor2.style.display = "none"
+            imgperp2.style.display = "none"
+            imgpernor3.style.display = "none"
+            imgperp3.style.display = "none"
+            imgpernor4.style.display = "none"
+            imgperp4.style.display = "none"
+
+
+        }, 1000)
+        setTimeout(() => {
+            imgpernordiv2.classList.remove("imgpernordivOnClick")
+        }, 500)
+    } else {
+        imgperdiv2.style.display = "flex"
+        document.body.style.overflow = "hidden"
+        imgpernor.style.display = "none"
+        imgperp.style.display = "none"
+        imgpernor2.style.display = "none"
+        imgperp2.style.display = "none"
+        imgpernor3.style.display = "none"
+        imgperp3.style.display = "none"
+        imgpernor4.style.display = "none"
+        imgperp4.style.display = "none"
+        
+    }
+}
+function minimizar2() {
+    let imgperampdiv2 = document.getElementById("imgperampdiv2")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgperampdiv2.classList.add("imgperampdivOnClick")
+        setTimeout(() => {
+            imgperdiv2.style.display = "none"
+            document.body.style.overflow = "visible"
+            imgperp.style.display = "inline-block"
+            imgpernor.style.display = "inline-block"
+            imgperp2.style.display = "inline-block"
+            imgpernor2.style.display = "inline-block"
+            imgperp3.style.display = "inline-block"
+            imgpernor3.style.display = "inline-block"
+            imgperp4.style.display = "inline-block"
+            imgpernor4.style.display = "inline-block"
+            
+        }, 1000)
+        setTimeout(() => {
+            imgperampdiv2.classList.remove("imgperampdivOnClick")
+        }, 500)
+    } else {
+        imgperdiv2.style.display = "none"
+        document.body.style.overflow = "visible"
+        imgperp.style.display = "inline-block"
+        imgpernor.style.display = "inline-block"
+        imgperp2.style.display = "inline-block"
+        imgpernor2.style.display = "inline-block"
+        imgperp3.style.display = "inline-block"
+        imgpernor3.style.display = "inline-block"
+        imgperp4.style.display = "inline-block"
+        imgpernor4.style.display = "inline-block"
+
+    }
+}
+function ampliar3() {
+    let imgpernordiv3 = document.getElementById("imgpernordiv3")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgpernordiv3.classList.add("imgpernordivOnClick")
+        setTimeout(() => {
+            imgperdiv3.style.display = "flex"
+             document.body.style.overflow = "hidden"
+            imgpernor.style.display = "none"
+            imgperp.style.display = "none"
+            imgpernor2.style.display = "none"
+            imgperp2.style.display = "none"
+            imgpernor3.style.display = "none"
+            imgperp3.style.display = "none"
+            imgpernor4.style.display = "none"
+            imgperp4.style.display = "none"
+
+
+        }, 1000)
+        setTimeout(() => {
+            imgpernordiv3.classList.remove("imgpernordivOnClick")
+        }, 500)
+    } else {
+        imgperdiv3.style.display = "flex"
+         document.body.style.overflow = "hidden"
+        imgpernor.style.display = "none"
+        imgperp.style.display = "none"
+        imgpernor2.style.display = "none"
+        imgperp2.style.display = "none"
+        imgpernor3.style.display = "none"
+        imgperp3.style.display = "none"
+        imgpernor4.style.display = "none"
+        imgperp4.style.display = "none"
+
+    }
+}
+function minimizar3() {
+    let imgperampdiv3 = document.getElementById("imgperampdiv3")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgperampdiv3.classList.add("imgperampdivOnClick")
+        setTimeout(() => {
+            imgperdiv3.style.display = "none"
+            document.body.style.overflow = "visible"
+            imgperp.style.display = "inline-block"
+            imgpernor.style.display = "inline-block"
+            imgperp2.style.display = "inline-block"
+            imgpernor2.style.display = "inline-block"
+            imgperp3.style.display = "inline-block"
+            imgpernor3.style.display = "inline-block"
+            imgperp4.style.display = "inline-block"
+            imgpernor4.style.display = "inline-block"
+
+        }, 1000)
+        setTimeout(() => {
+            imgperampdiv3.classList.remove("imgperampdivOnClick")
+        }, 500)
+    } else {
+        imgperdiv3.style.display = "none"
+        document.body.style.overflow = "visible"
+        imgperp.style.display = "inline-block"
+        imgpernor.style.display = "inline-block"
+        imgperp2.style.display = "inline-block"
+        imgpernor2.style.display = "inline-block"
+        imgperp3.style.display = "inline-block"
+        imgpernor3.style.display = "inline-block"
+        imgperp4.style.display = "inline-block"
+        imgpernor4.style.display = "inline-block"
+
+    }
+}
+function ampliar4() {
+    let imgpernordiv4 = document.getElementById("imgpernordiv4")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgpernordiv4.classList.add("imgpernordivOnClick")
+        setTimeout(() => {
+            imgperdiv4.style.display = "flex"
+             document.body.style.overflow = "hidden"
+            imgpernor.style.display = "none"
+            imgperp.style.display = "none"
+            imgpernor2.style.display = "none"
+            imgperp2.style.display = "none"
+            imgpernor3.style.display = "none"
+            imgperp3.style.display = "none"
+            imgpernor4.style.display = "none"
+            imgperp4.style.display = "none"
+
+
+        }, 1000)
+        setTimeout(() => {
+            imgpernordiv4.classList.remove("imgpernordivOnClick")
+        }, 500)
+    } else {
+        imgperdiv4.style.display = "flex"
+         document.body.style.overflow = "hidden"
+        imgpernor.style.display = "none"
+        imgperp.style.display = "none"
+        imgpernor2.style.display = "none"
+        imgperp2.style.display = "none"
+        imgpernor3.style.display = "none"
+        imgperp3.style.display = "none"
+        imgpernor4.style.display = "none"
+        imgperp4.style.display = "none"
+
+    }
+}
+function minimizar4() {
+    let imgperampdiv4 = document.getElementById("imgperampdiv4")
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        imgperampdiv4.classList.add("imgperampdivOnClick")
+        setTimeout(() => {
+            imgperdiv4.style.display = "none"
+            document.body.style.overflow = "visible"
+            imgperp.style.display = "inline-block"
+            imgpernor.style.display = "inline-block"
+            imgperp2.style.display = "inline-block"
+            imgpernor2.style.display = "inline-block"
+            imgperp3.style.display = "inline-block"
+            imgpernor3.style.display = "inline-block"
+            imgperp4.style.display = "inline-block"
+            imgpernor4.style.display = "inline-block"
+
+        }, 1000)
+        setTimeout(() => {
+            imgperampdiv4.classList.remove("imgperampdivOnClick")
+        }, 500)
+    } else {
+        imgperdiv4.style.display = "none"
+        document.body.style.overflow = "visible"
+        imgperp.style.display = "inline-block"
+        imgpernor.style.display = "inline-block"
+        imgperp2.style.display = "inline-block"
+        imgpernor2.style.display = "inline-block"
+        imgperp3.style.display = "inline-block"
+        imgpernor3.style.display = "inline-block"
+        imgperp4.style.display = "inline-block"
+        imgpernor4.style.display = "inline-block"
+
+    }
 }
--- js/script.js
+++ js/script.js
@@ -27,24 +27,268 @@ var swiper = new Swiper(".slide-content", {
     },
 });
 
+var corpo = document.getElementById("corpo")
+setTimeout(() => {
+    corpo.style.animation = "fundoanimado 30s linear infinite"
+}, 1000)
+
 function carregando() {
     setTimeout(() => {
         document.getElementById("pcarregar").style.display = "none"
         document.getElementById("carregar").style.display = "none"
         document.getElementById("bcarregar").style.display = "block"
-    }, "1000");
+    }, 1000);
 }
 
-var audio = document.getElementById("myAudio");
 function ace() {
-    document.getElementById("conteudo").style.display = "block"
-    document.getElementById("carregabox").style.display = "none"
-    audio.volume = .3
-    audio.play()
+    var bcarregar = document.getElementById("bcarregar")
+    var conteudo = document.getElementById("conteudo")
+    var carregabox = document.getElementById("carregabox")
+    podcastAudio.volume = .3
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        bcarregar.classList.add("bcarregarOnClick")
+        setTimeout(() => {
+            conteudo.style.display = "block"
+            carregabox.style.display = "none"
+            podcastAudio.play()
+        }, 1000)
+        setTimeout(() => {
+            bcarregar.classList.remove("bcarregarOnClick")
+        }, 500)
+    } else {
+        conteudo.style.display = "block"
+        carregabox.style.display = "none"
+        podcastAudio.play()
+    }
 }
 
-var corpo = document.getElementById("corpo")
-setTimeout(() => { 
-    corpo.style.animation = "fundoanimado 30s linear infinite"
-}, "1000")
+var podcastAudio = document.getElementById('podcast-audio')
+var playBtn = document.getElementById('podcast-play')
+var pauseBtn = document.getElementById('podcast-pause')
+var playShow = function () {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        playBtn.classList.add("podOnClick")
+        setTimeout(() => {
+            podcastAudio.play()
+            playBtn.style.display = "none"
+            pauseBtn.style.display = "inline-block"
+        }, 1000);
+        setTimeout(() => {
+            playBtn.classList.remove("podOnClick")
+        }, 1050)
+    } else {
+        podcastAudio.play()
+        playBtn.style.display = "none"
+        pauseBtn.style.display = "inline-block"
+    }
+}
+
+var pauseShow = function () {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        pauseBtn.classList.add("podOnClick")
+        setTimeout(() => {
+            podcastAudio.pause()
+            playBtn.style.display = "inline-block"
+            pauseBtn.style.display = "none"
+        }, 1000);
+        setTimeout(() => {
+            pauseBtn.classList.remove("podOnClick")
+        }, 500);
+    } else {
+        podcastAudio.pause()
+        playBtn.style.display = "inline-block"
+        pauseBtn.style.display = "none"
+    }
+}
+
+
+function big() {
+    var big1 = document.getElementById("big")
+    var des = document.getElementsByClassName("description")[1]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        big1.classList.add("bigrel")
+        des.classList.add("bigreldes")
+        setTimeout(() => {
+            window.open('html/big.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            big1.classList.remove("bigrel")
+            des.classList.remove("bigreldes")
+        }, 500)
+    } else {
+        window.open('html/big.html', '_blank')
+    }
+}
+
+
+function relf() {
+    var rel1 = document.getElementById("rel")
+    var des = document.getElementsByClassName("description")[6]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        rel1.classList.add("bigrel")
+        des.classList.add("bigreldes")
+        setTimeout(() => {
+            window.open('html/rel.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            rel1.classList.remove("bigrel")
+            des.classList.remove("bigreldes")
+        }, 500)
+    } else {
+        window.open('html/rel.html', '_blank')
+    }
+}
+function com() {
+    var com1 = document.getElementById("com")
+    var des = document.getElementsByClassName("description")[2]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        com1.classList.add("comhem")
+        des.classList.add("comhemdes")
+        setTimeout(() => {
+            window.open('html/com.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            rel1.classList.remove("comhem")
+            des.classList.remove("comhemdes")
+        }, 500)
+    } else {
+        window.open('html/com.html', '_blank')
+    }
+}
+function hem() {
+    var hem1 = document.getElementById("hem")
+    var des = document.getElementsByClassName("description")[7]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        hem1.classList.add("comhem")
+        des.classList.add("comhemdes")
+        setTimeout(() => {
+            window.open('html/hem.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            hem1.classList.remove("comhem")
+            des.classList.remove("comhemdes")
+        }, 500)
+    } else {
+        window.open('html/hem.html', '_blank')
+    }
+}
+function pla() {
+    var pla1 = document.getElementById("pla")
+    var des = document.getElementsByClassName("description")[3]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        pla1.classList.add("plalua")
+        des.classList.add("plaluades")
+        setTimeout(() => {
+            window.open('html/pla.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            pla1.classList.remove("plalua")
+            des.classList.remove("plaluades")
+        }, 500)
+    } else {
+        window.open('html/pla.html', '_blank')
+    }
+}
+function lua() {
+    var lua1 = document.getElementById("lua")
+    var des = document.getElementsByClassName("description")[8]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        lua1.classList.add("plalua")
+        des.classList.add("plaluades")
+        setTimeout(() => {
+            window.open('html/lua.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            lua1.classList.remove("plalua")
+            des.classList.remove("plaluades")
+        }, 500)
+    } else {
+        window.open('html/lua.html', '_blank')
+    }
+}
+function ast() {
+    var ast1 = document.getElementById("ast")
+    var des = document.getElementsByClassName("description")[4]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        ast1.classList.add("astecl")
+        des.classList.add("astecldes")
+        setTimeout(() => {
+            window.open('html/ast.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            ast1.classList.remove("astecl")
+            des.classList.remove("astecldes")
+        }, 500)
+    } else {
+        window.open('html/ast.html', '_blank')
+    }
+}
+function eclf() {
+    var ecl1 = document.getElementById("ecl")
+    var des = document.getElementsByClassName("description")[9]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        ecl1.classList.add("astecl")
+        des.classList.add("astecldes")
+        setTimeout(() => {
+            window.open('html/ecl.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            ecl1.classList.remove("astecl")
+            des.classList.remove("astecldes")
+        }, 500)
+    } else {
+        window.open('html/ecl.html', '_blank')
+    }
+}
+function mov() {
+    var mov1 = document.getElementById("mov")
+    var des = document.getElementsByClassName("description")[5]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        mov1.classList.add("movinf")
+        des.classList.add("movinfdes")
+        setTimeout(() => {
+            window.open('html/mov.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            mov1.classList.remove("movinf")
+            des.classList.remove("movinfdes")
+        }, 500)
+    } else {
+        window.open('html/mov.html', '_blank')
+    }
+}
+function inf() {
+    var inf1 = document.getElementById("inf")
+    var des = document.getElementsByClassName("description")[10]
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        inf1.classList.add("movinf")
+        des.classList.add("movinfdes")
+        setTimeout(() => {
+            window.open('html/inf.html', '_blank')
+        }, 1000)
+        setTimeout(() => {
+            inf1.classList.remove("movinf")
+            des.classList.remove("movinfdes")
+        }, 500)
+    } else {
+        window.open('html/inf.html', '_blank')
+    }
+}
+
+function logof() {
+    var logo = document.getElementById("logo")
+    if (window.matchMedia("(max-width: 400px)").matches) {
+        logo.classList.add("logoOnClick")
+        setTimeout(() => {
+            window.open('https://github.com/mwd-oficial', '_blank')
+        }, 1000);
+        setTimeout(() => {
+            logo.classList.remove("logoOnClick")
+        }, 1050);
+    } else {
+        window.open('https://github.com/mwd-oficial', '_blank')
+
+    }
+
+}
 
